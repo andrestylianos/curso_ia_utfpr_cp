4058a0d005590840e5fd2617850e9d47f50b671c
andrestylianos <andre.stylianos@gmail.com>
2014-04-28 02:51:30 -0300

    Classe de manipulacao dos nos folhas

---
+++ Trabalho 1 - Pacman/MapaFolhas.java
@@ -0,0 +1,32 @@
+package player;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class MapaFolhas {
+	HashMap <Node,Double> mapavalores = new HashMap<Node,Double>();
+
+	public void adicionaFolha(Node folha){
+		mapavalores.put(folha, folha.heuristica);
+	}
+
+	public void substituiFolha(Node folha, DFSPacManPlayer player){
+		mapavalores.remove(folha);
+		folha.nodes = folha.criaArv(folha.estadoatual, player, this);
+		for(Node n : folha.nodes){
+			mapavalores.put(n, n.heuristica);
+		}
+	}
+	
+	public Node retornaMaior(){
+		double max = Double.NEGATIVE_INFINITY;
+		Node melhorescolha=null;
+		for(Node folha : mapavalores.keySet()){
+			if(mapavalores.get(folha)>=max){
+				max = mapavalores.get(folha);
+				melhorescolha=folha;
+			}
+		}
+		return melhorescolha;
+	}
+}
