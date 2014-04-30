abf1b9217f5afae9076789bf100773fb6a2894c6
andrestylianos <andre.stylianos@gmail.com>
2014-04-29 21:20:08 -0300

    Adicionados comentarios

--- Trabalho 1 - Pacman/DFSPacManPlayer.java
+++ Trabalho 1 - Pacman/DFSPacManPlayer.java
@@ -5,52 +5,55 @@ import java.util.List;
 import pacman.*;
 
 public class DFSPacManPlayer implements PacManPlayer, StateEvaluator {
-    
-    static int nivel = 10;
-    public Move lastmove = null;
-    static boolean Aestrela = true;
 
-    public Move getLastmove() {
-        return lastmove;
-    }
-    
-  @Override
-  public Move chooseMove(Game game){
-	  MapaFolhas map = new MapaFolhas();
-	  Node arvore = new Node(game.getCurrentState(),this, map);
-          lastmove=arvore.retornaJogada(map.retornaMaior());
-          return lastmove;
-  }
+	static int nivel = 3; // Profundidade maxima da arvore
+	public Move lastmove = null;
+	static boolean Aestrela = true; // Ativa ou desativa a busca A*
 
-  @Override
-  public double evaluateState(State state) {
-	  double value = 0; 
-	  if(Game.isFinal(state)){
-		  if(Game.isWinning(state)){
-			  return 100000;
-		  }
-		  if (Game.isLosing(state)){
-			  return -100000000;
-		  }
-	  }
-	  Location pacmanlocal = state.getPacManLocation();
-	  List<Location> ghostlocal = state.getGhostLocations();
-	  LocationSet dots = state.getDotLocations();
-	  AvaliacaoHeuristica avalia = new AvaliacaoHeuristica();
-	  value+= (avalia.closestEuclideanGhost(pacmanlocal, ghostlocal)*0.5);
-	  value+= (avalia.closestManhattanGhost(pacmanlocal, ghostlocal)*0.8);
-	  value+= (avalia.averageEuclideanGhost(pacmanlocal, ghostlocal)*0.15);
-	  value+= (avalia.averageManhattanGhost(pacmanlocal, ghostlocal)*0.15);   
-	  value+= (avalia.numberDots(dots)*0.5);
-	  value+= (avalia.secondEuclideanGhost(pacmanlocal, ghostlocal)*0.15);
-	  value+= (avalia.secondManhattanGhost(pacmanlocal, ghostlocal)*0.3);
-	  value+= (avalia.averageEuclideanDot(pacmanlocal, dots)*0.05);
-	  value+= (avalia.averageManhattanDot(pacmanlocal, dots)*0.1);
-	  value+= (avalia.closestDotEuclidean(pacmanlocal, dots)*0.3);
-	  value+= (avalia.closestDotManhattan(pacmanlocal, dots)*0.6);
+	// Retorna o ultimo movimento realizado pelo pacman no jogo.
+	public Move getLastmove() {
+		return lastmove;
+	}
 
-	  return value;
-  }
+	@Override
+	public Move chooseMove(Game game){
+		MapaFolhas map = new MapaFolhas();
+		Node arvore = new Node(game.getCurrentState(),this, map);
+		lastmove=arvore.retornaJogada(map.retornaMaior());
+		return lastmove;
+	}
+
+	
+	//Realiza o calculo da heuristica total de um estado utilizando as heuristicas parciais e seus respectivos pesos.
+	@Override
+	public double evaluateState(State state) {
+		double value = 0; 
+		if(Game.isFinal(state)){
+			if(Game.isWinning(state)){
+				return 100000;
+			}
+			if (Game.isLosing(state)){
+				return -100000000;
+			}
+		}
+		Location pacmanlocal = state.getPacManLocation();
+		List<Location> ghostlocal = state.getGhostLocations();
+		LocationSet dots = state.getDotLocations();
+		AvaliacaoHeuristica avalia = new AvaliacaoHeuristica();
+		value+= (avalia.closestEuclideanGhost(pacmanlocal, ghostlocal)*1);
+		value+= (avalia.closestManhattanGhost(pacmanlocal, ghostlocal)*1);
+		value+= (avalia.averageEuclideanGhost(pacmanlocal, ghostlocal)*1);
+		value+= (avalia.averageManhattanGhost(pacmanlocal, ghostlocal)*1);   
+		value+= (avalia.numberDots(dots)*1);
+		value+= (avalia.secondEuclideanGhost(pacmanlocal, ghostlocal)*1);
+		value+= (avalia.secondManhattanGhost(pacmanlocal, ghostlocal)*1);
+		value+= (avalia.averageEuclideanDot(pacmanlocal, dots)*1);
+		value+= (avalia.averageManhattanDot(pacmanlocal, dots)*1);
+		value+= (avalia.closestDotEuclidean(pacmanlocal, dots)*1);
+		value+= (avalia.closestDotManhattan(pacmanlocal, dots)*1);
+
+		return value;
+	}
 
 
 }
