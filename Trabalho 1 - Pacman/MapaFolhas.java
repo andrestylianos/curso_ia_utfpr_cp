4ed71ac123515c93e868e8020b7d1a529f6c6bc1
andrestylianos <andre.stylianos@gmail.com>
2014-04-29 21:20:17 -0300

    Adicionados comentarios

--- Trabalho 1 - Pacman/MapaFolhas.java
+++ Trabalho 1 - Pacman/MapaFolhas.java
@@ -6,10 +6,14 @@ import java.util.HashMap;
 public class MapaFolhas {
 	HashMap <Node,Double> mapavalores = new HashMap<Node,Double>();
 
+	// Adiciona uma folha (chave) e sua heuristica (valor) no hashmap
 	public void adicionaFolha(Node folha){
 		mapavalores.put(folha, folha.heuristica);
 	}
 
+	// Para uso com a busca A*
+	// É passado o nó com o valor heuristico mais alto que será expandido.
+	// Remove o nó que foi passado, expande na arvore e adiciona seus filhos no hashmap
 	public void substituiFolha(Node folha, DFSPacManPlayer player){
 		mapavalores.remove(folha);
 		folha.nodes = folha.criaArv(folha.estadoatual, player, this);
@@ -17,7 +21,8 @@ public class MapaFolhas {
 			mapavalores.put(n, n.heuristica);
 		}
 	}
-	
+
+	// Retorna o nó de maior valor contido no hashmap
 	public Node retornaMaior(){
 		double max = Double.NEGATIVE_INFINITY;
 		Node melhorescolha=null;
