ed23204b7bddfe14bedc1126628fa43d034d14af
André Ramos <andre.stylianos@gmail.com>
2014-07-12 23:44:15 -0300

    modify PacManProblem to work with new pacman packages

--- Ecj/src/ec/Pacman/PacManProblem.java
+++ Ecj/src/ec/Pacman/PacManProblem.java
@@ -6,17 +6,15 @@
 
 
 package ec.Pacman;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 import ec.*;
 import ec.simple.*;
 import ec.vector.*;
+import ec.Pacman.pacman.*;
 
 public class PacManProblem extends Problem implements SimpleProblemForm
     {
+
     public void evaluate(final EvolutionState state,
         final Individual ind,
         final int subpopulation,
@@ -31,40 +29,22 @@ public class PacManProblem extends Problem implements SimpleProblemForm
         int pontos=0;
         int valorfitness=0;
         int level=0;
-        
-        Process pacman = null;
-		try {
-			//pacman = Runtime.getRuntime().exec("java -cp workspace/EvoPacman/bin/ pacman.Game -pacman player.DFSPacManPlayer -display none -ghosts ghosts.RandomGhostPlayer,ghosts.RandomGhostPlayer,ghosts.RandomGhostPlayer,ghosts.RandomGhostPlayer -heuristicas 1,1,1,1,1,1,1,1,1,1,1,0,0");
-			String individuo,stringheuristica;
-			individuo = ind.genotypeToStringForHumans();
-			stringheuristica = (individuo.charAt(0)+","+individuo.charAt(1)+","+individuo.charAt(2)+","+individuo.charAt(3)+","+individuo.charAt(4)+","+individuo.charAt(5)+","+individuo.charAt(6)+","+individuo.charAt(7)+","+individuo.charAt(8)+","+individuo.charAt(9)+","+individuo.charAt(10)+","+individuo.charAt(11)+","+individuo.charAt(12));
-			ProcessBuilder builder = new ProcessBuilder("java","-cp","workspace/codigos_IA/PacmanEvo/bin/", "pacman.Game", "-pacman", "player.DFSPacManPlayer", "-display", "none", "-ghosts", "ghosts.RandomGhostPlayer,ghosts.RandomGhostPlayer,ghosts.RandomGhostPlayer,ghosts.RandomGhostPlayer","-levels","10", "-heuristicas",stringheuristica);
-			builder.redirectErrorStream(true);
-			builder.directory(new File("/home/pantera"));
-			pacman = builder.start();
-		} catch (IOException e) {
-	         System.err.println("Error on exec() method");
-	         e.printStackTrace();  
+        int[] stats = null, heuristica = new int[13];
+        String individuo,stringheuristica;
+		individuo = ind.genotypeToStringForHumans();
+		stringheuristica = (individuo.charAt(0)+","+individuo.charAt(1)+","+individuo.charAt(2)+","+individuo.charAt(3)+","+individuo.charAt(4)+","+individuo.charAt(5)+","+individuo.charAt(6)+","+individuo.charAt(7)+","+individuo.charAt(8)+","+individuo.charAt(9)+","+individuo.charAt(10)+","+individuo.charAt(11)+","+individuo.charAt(12));
+		for(int i=0;i<individuo.length();i++){
+			heuristica[i]= Character.getNumericValue(stringheuristica.charAt(i));
 		}
-		BufferedReader input = new BufferedReader(new InputStreamReader(pacman.getInputStream()));
 		try {
-			for (String line = input.readLine(); line != null; line = input.readLine()) {
-				if(line.startsWith("Time")){
-					tempo=Integer.parseInt(line.split(":   ")[1]);
-				}
-				if(line.startsWith("Points")){
-					pontos=Integer.parseInt(line.split(": ")[1]);
-				}
-				if(line.startsWith("Level")){
-					level=Integer.parseInt(line.split(": ")[1]);
-				}
-			}
-
-			input.close();
-		} catch (IOException e) {
-	         System.err.println("Error on inStream.readLine()");
-	         e.printStackTrace();  
+			stats = Game.init(10,"none","ec.Pacman.ghosts.RandomGhostPlayer,ec.Pacman.ghosts.StalkingGhostPlayer,ec.Pacman.ghosts.RandomGhostPlayer,ec.Pacman.ghosts.RandomGhostPlayer",heuristica);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		pontos=stats[0];
+		level=stats[1];
+		tempo=stats[2];
         valorfitness=(pontos+tempo)*level;
         BitVectorIndividual ind2 = (BitVectorIndividual)ind;
         
